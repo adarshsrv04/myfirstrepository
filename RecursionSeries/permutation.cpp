bf0f4b9670569222a682897db1e27156c3e174c2
Adarsh Kumar <65950122+adarshsrv04@users.noreply.github.com>
2021-02-10 21:06:25 +0530

    Add files via upload

--- RecursionSeries/permutation.cpp
+++ RecursionSeries/permutation.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+void permutation(string ques, string ans) {
+    if(ques.length()==0) {
+        cout << ans << "\n";
+        return;
+    }
+    for(int i=0; i<ques.length(); i++) {
+        permutation(ques.substr(0, i) + ques.substr(i+1), ans+ques[i]);
+    }
+}
+
+int main() {
+    string str;
+    cin >> str;
+    permutation(str, "");
+	return 0;
+}
