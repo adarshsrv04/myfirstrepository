a0ca91c0eb034ca833aab5782c7e34e5435a2a84
Adarsh Kumar <65950122+adarshsrv04@users.noreply.github.com>
2021-02-20 13:40:18 +0530

    Create validAnagram.cpp

---
+++ TutorialCup/validAnagram.cpp
@@ -0,0 +1,21 @@
+//Valid Anagram
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    if(s1.length() != s2.length()) {
+        cout << "false"; return 0;
+    }
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    if(s1 == s2) {
+        cout << "true"; return 0;
+    }
+    else {
+        cout << "false"; return 0;
+    }
+	return 0;
+}
